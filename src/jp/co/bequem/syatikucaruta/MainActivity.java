35a202904fa2123d341471a35c2d97cd224cc04c
butyutyumpa <butyutyumpa@gmail.com>
2014-02-23 17:23:09 +0900

    Annotationsを本当に使えるように

--- src/jp/co/bequem/syatikucaruta/MainActivity.java
+++ src/jp/co/bequem/syatikucaruta/MainActivity.java
@@ -7,12 +7,4 @@ import android.app.Activity;
 import android.view.Menu;
 @EActivity(R.layout.activity_main)
 public class MainActivity extends Activity {
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
 }
